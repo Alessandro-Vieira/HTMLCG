f8606b7012f22757824ceb8b9271e4109b1b4bba
Alessandro-Vieira <47628979+Alessandro-Vieira@users.noreply.github.com>
2022-06-08 20:21:35 -0300

    Add files via upload

--- PROJETO/js/funcao.js
+++ PROJETO/js/funcao.js
@@ -1,18 +1,15 @@
+function mascara_tel(objeto){
+    if(objeto.value.length == 0){
+        objeto.value = objeto.value + '(';
+    }
+    if(objeto.value.length == 3){
+        objeto.value = objeto.value + ')';
+    }
+    if(objeto.value.length == 9){
+        objeto.value = objeto.value + '-';
+    }
+}
+
 function msg(){
     alert("Sr(a) " + document.faleconosco.nome.value + ", seus dados foram enviados com sucesso.");
-}
-
-function aumento(){
-    document.faleconosco.nome.value = document.faleconosco.nome.value.toUpperCase();
-}
-
-function acesso(){
-    if (document.restrito.cpf.value=="024" && document.restrito.senha.value=="123"){
-        alert("Seja bem vindo ao Sistema Administrativo");
-    }
-    else{
-        alert("Sr(a) verifique as suas credenciais, caso seja o primeiro acesso, clicar sobre o botão Cadastrar");
-    }
-    document.restrito.cpf.value="";
-    document.restrito.senha.value="";
 }
